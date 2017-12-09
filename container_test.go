cbe7be7678043ee9a0b9bb28b5a4e8a27e28d981
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2017-12-09 14:47:32 +0100

    * ADD: New unit test

--- container_test.go
+++ container_test.go
@@ -10,26 +10,44 @@ import (
 const (
     floatArrayParameter     = "float_array_parameter"
     floatParameter          = "float_parameter"
+    float64ArrayParameter   = "float64_array_parameter"
+    float64Parameter        = "float64_parameter"
     intArrayParameter       = "int_array_parameter"
     intParameter            = "int_parameter"
+    int64ArrayParameter     = "int64_array_parameter"
+    int64Parameter          = "int64_parameter"
     stringArrayParameter    = "string_array_parameter"
     stringParameter         = "string_parameter"
+    boolParameter           = "bool_parameter"
+    boolArrayParameter      = "bool_array_parameter"
     notExistParameter       = "not_exists"
-    sqlConnection           = "host=localhost user=user password=password dbname=iot sslmode=disable"
+    sqlConnection           = "host=localhost user=user password=password dbname=default sslmode=disable"
     testDatabaseName        = "testing"
     nonExistingDatabaseName = "not_exists"
 )
 
 var testParameters = map[string]interface{}{
-    floatArrayParameter:  []float32{-3, -2, -1, 0, 1, 2, 3},
-    floatParameter:       100.01,
-    intArrayParameter:    []int{-3, -2, -1, 0, 1, 2, 3},
-    intParameter:         100,
-    stringArrayParameter: []string{"a", "b", "c"},
-    stringParameter:      "string_parameter",
+    boolParameter:         true,
+    boolArrayParameter:    []bool{true, false, true, true},
+    floatArrayParameter:   []float32{-3, -2, -1, 0, 1, 2, 3},
+    floatParameter:        float32(100.01),
+    float64ArrayParameter: []float64{-3, -2, -1, 0, 1, 2, 3},
+    float64Parameter:      float64(100.01),
+    intArrayParameter:     []int{-3, -2, -1, 0, 1, 2, 3},
+    intParameter:          100,
+    int64ArrayParameter:   []int64{-3, -2, -1, 0, 1, 2, 3},
+    int64Parameter:        int64(1),
+    stringArrayParameter:  []string{"a", "b", "c"},
+    stringParameter:       "string_parameter",
 }
 
 func TestGetContainer(t *testing.T) {
+    assert.Panics(t, func() {
+        GetContainer()
+    })
+
+    Initialize(logrus.New())
+
     assert.Equal(t, containerObject, GetContainer())
 }
 
@@ -133,4 +151,100 @@ func TestContainer_GetLogger(t *testing.T) {
     loggerInContainer := containerObject.GetLogger()
 
     assert.Equal(t, logger, loggerInContainer)
+}
+
+func TestContainer_GetBoolArrayParameter(t *testing.T) {
+    containerObject.SetParameter(boolArrayParameter, testParameters[boolArrayParameter])
+
+    parameter := containerObject.GetBoolArrayParameter(boolArrayParameter)
+
+    assert.Equal(t, testParameters[boolArrayParameter], parameter)
+}
+
+func TestContainer_GetBoolParameter(t *testing.T) {
+    containerObject.SetParameter(boolParameter, testParameters[boolParameter])
+
+    parameter := containerObject.GetBoolParameter(boolParameter)
+
+    assert.Equal(t, testParameters[boolParameter], parameter)
+}
+
+func TestContainer_GetFloatArrayParameter(t *testing.T) {
+    containerObject.SetParameter(floatArrayParameter, testParameters[floatArrayParameter])
+
+    parameter := containerObject.GetFloatArrayParameter(floatArrayParameter)
+
+    assert.Equal(t, testParameters[floatArrayParameter], parameter)
+}
+
+func TestContainer_GetFloatParameter(t *testing.T) {
+    containerObject.SetParameter(floatParameter, testParameters[floatParameter])
+
+    parameter := containerObject.GetFloatParameter(floatParameter)
+
+    assert.Equal(t, testParameters[floatParameter], parameter)
+}
+
+func TestContainer_GetFloat64ArrayParameter(t *testing.T) {
+    containerObject.SetParameter(float64ArrayParameter, testParameters[float64ArrayParameter])
+
+    parameter := containerObject.GetFloat64ArrayParameter(float64ArrayParameter)
+
+    assert.Equal(t, testParameters[float64ArrayParameter], parameter)
+}
+
+func TestContainer_GetFloat64Parameter(t *testing.T) {
+    containerObject.SetParameter(float64Parameter, testParameters[float64Parameter])
+
+    parameter := containerObject.GetFloat64Parameter(float64Parameter)
+
+    assert.Equal(t, testParameters[float64Parameter], parameter)
+}
+
+func TestContainer_GetIntArrayParameter(t *testing.T) {
+    containerObject.SetParameter(intArrayParameter, testParameters[intArrayParameter])
+
+    parameter := containerObject.GetIntArrayParameter(intArrayParameter)
+
+    assert.Equal(t, testParameters[intArrayParameter], parameter)
+}
+
+func TestContainer_GetIntParameter(t *testing.T) {
+    containerObject.SetParameter(intParameter, testParameters[intParameter])
+
+    parameter := containerObject.GetIntParameter(intParameter)
+
+    assert.Equal(t, testParameters[intParameter], parameter)
+}
+
+func TestContainer_GetInt64ArrayParameter(t *testing.T) {
+    containerObject.SetParameter(int64ArrayParameter, testParameters[int64ArrayParameter])
+
+    parameter := containerObject.GetInt64ArrayParameter(int64ArrayParameter)
+
+    assert.Equal(t, testParameters[int64ArrayParameter], parameter)
+}
+
+func TestContainer_GetInt64Parameter(t *testing.T) {
+    containerObject.SetParameter(int64Parameter, testParameters[int64Parameter])
+
+    parameter := containerObject.GetInt64Parameter(int64Parameter)
+
+    assert.Equal(t, testParameters[int64Parameter], parameter)
+}
+
+func TestContainer_GetStringArrayParameter(t *testing.T) {
+    containerObject.SetParameter(stringArrayParameter, testParameters[stringArrayParameter])
+
+    parameter := containerObject.GetStringArrayParameter(stringArrayParameter)
+
+    assert.Equal(t, testParameters[stringArrayParameter], parameter)
+}
+
+func TestContainer_GetStringParameter(t *testing.T) {
+    containerObject.SetParameter(stringParameter, testParameters[stringParameter])
+
+    parameter := containerObject.GetStringParameter(stringParameter)
+
+    assert.Equal(t, testParameters[stringParameter], parameter)
 }
